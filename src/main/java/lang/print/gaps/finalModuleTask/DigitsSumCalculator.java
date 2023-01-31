4c7398d445e4b577ee34c580875ff707b6f159b7
IrinaSelitskaya <119079534+IrinaSelitskaya@users.noreply.github.com>
2023-01-31 11:13:00 +0300

    Update DigitsSumCalculator.java

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,13 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+     number=1234;
+     int fourth=number%10;
+     int number2=number/10;
+     int third=number2%10;
+     int number3=number2/10;
+     int second=number3%10;
+     int first=number3/10;
+     System.out.println(fourth+third+second+first);
     }
 }
